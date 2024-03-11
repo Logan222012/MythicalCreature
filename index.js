57cc534370bcc9e5c4d862876c6f6a38d25030a5
Logan222012 <158110719+Logan222012@users.noreply.github.com>
2024-03-11 18:35:39 -0500

    Work on minigame
    Cheese

---
+++ index.js
@@ -0,0 +1,7 @@
+const backgroundmusic = new Audio ("backgroundmusic.mp3")
+backgroundmusic.load()
+backgroundmusic.play()
+function load() {
+    backgroundmusic.load()
+    backgroundmusic.play()
+}
--- minigame.js
+++ minigame.js
@@ -1,3 +1,9 @@
 let move_speed = 3;
 let gravity = 0.5;
-let bird = document.
+let bird = document.getElementsByClassName("bird");
+let background = document.getElementsByClassName("background");
+let pipes = document.getElementsByClassName("pipes");
+window.addEventListener("click", fly)
+function fly() {
+    bird.className += " birdfly";
+}

42dba30e7ca2253cf46876bc2153731a3d446235
mikexie360 <33299123+mikexie360@users.noreply.github.com>
2024-03-16 10:59:20 -0500

    Working on game and changing to precents

--- minigame.js
+++ minigame.js
@@ -3,7 +3,11 @@ let gravity = 0.5;
 let bird = document.getElementsByClassName("bird");
 let background = document.getElementsByClassName("background");
 let pipes = document.getElementsByClassName("pipes");
-window.addEventListener("click", fly)
-function fly() {
-    bird.className += " birdfly";
+window.addEventListener("click", fly);
+bird.onclick = function fly() {
+    bird.className += "birdfly";
+};
+for (let i = 0;  i < 10; i++) {
+   
+    document.addEventListener("click", fly);
 }

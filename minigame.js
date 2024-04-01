70ecaf662f90ee114b0f0efa3d28e941a7783031
Logan222012 <158110719+Logan222012@users.noreply.github.com>
2024-04-01 17:47:37 -0500

    Worked on lesson & formating

--- minigame.js
+++ minigame.js
@@ -9,27 +9,27 @@ let score_title = document.querySelector(".score_title");
 let score_val = document.querySelector(".score_val");
 let game_has_started = false;
 document.addEventListener("keydown", (event) => {
-    if(event.key == 'Enter' && game_has_started == false){
+    if (event.key == 'Enter' && game_has_started == false) {
         document.querySelectorAll('.pipes').forEach((event) => {
-            event.remove(); 
+            event.remove();
         });
-      event.bird.top = '40vh';
-      game_has_started = 'Play'
-      message.innerHTML = '';
-      score_title.innerHTML = 'Score : ';
-      score_val.innerHTML = '0';
-      play();
-    }   
+        event.bird.top = '40vh';
+        game_has_started = 'Play'
+        message.innerHTML = '';
+        score_title.innerHTML = 'Score : ';
+        score_val.innerHTML = '0';
+        play();
+    }
 });
 function play() {
     function move() {
         if (game_has_started != 'Play') return;
         let pipes = document.querySelectorAll('.pipes')
-        pipes.forEach( (pipe) => {
+        pipes.forEach((pipe) => {
             let pipes_hitbox = pipe.getBoundingClientRect();
-            let bird_hitbox = bird.getBoundingClientRect(); 
+            let bird_hitbox = bird.getBoundingClientRect();
             if (pipes_hitbox.right <= 0) {
-            bird.remove();
+                bird.remove();
             } else {
                 if (
                     bird_hitbox.left < pipes_hitbox.left +
@@ -48,7 +48,7 @@ function play() {
                 } else {
                     if (
                         pipes_hitbox.right < bird_hitbox.left &&
-                        pipes_hitbox.right + 
+                        pipes_hitbox.right +
                         move_speed >= bird_hitbox.left &&
                         pipe.increase_score === '1'
                     ) {
@@ -58,7 +58,7 @@ function play() {
                 }
             }
         });
-          requestAnimationFrame(move);
+        requestAnimationFrame(move);
     }
     requestAnimationFrame(move);
     let bird_dy = 0;
@@ -66,16 +66,16 @@ function play() {
         if (game_has_started != 'Play') return;
         bird_dy = bird_dy + gravity;
         document.addEventListener('keydown', (e) => {
-            if(e.key == 'ArrowUp' || e.key == '') {
+            if (e.key == 'ArrowUp' || e.key == '') {
                 bird_dy = -7.6;
             }
         });
-      if (bird_hitbox.top <= 0 || 
-        bird_hitbox.bottom >= background.bottom) {
-    game_has_started = 'End';
-    message.innerHTML = 'Press Enter To Restart';
-    message.style.left = '28vw';
-    return;
+        if (bird_hitbox.top <= 0 ||
+            bird_hitbox.bottom >= background.bottom) {
+            game_has_started = 'End';
+            message.innerHTML = 'Press Enter To Restart';
+            message.style.left = '28vw';
+            return;
         }
         bird.style.top = bird_hitbox.style.top + bird_dy + 'px';
         bird_hitbox = bird.getBoundingClientRect();
@@ -85,9 +85,10 @@ function play() {
     let pipe_separation = 0;
     let pipe_gap = 40;
     function create_pipe() {
-        if (game_has_started !=  'Play') return;
+        if (game_has_started != 'Play') return;
         if (pipe_separation > 115) {
-            pipe_separation 0;
+            pipe_separation 0
+            let pipe_posi = Math.floor(Math.random() *  )
         }
     }
 }
